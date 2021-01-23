76ee0e3b9d0d881bbef9e7a64e2ac14844d079a9
Jake <36766388+JakeNTech@users.noreply.github.com>
2021-01-23 21:12:14 +0000

    Even more better
    Blog is somewhat new....

---
+++ assets/js/js_markdown_blog.js
@@ -0,0 +1,81 @@
+//V2
+//JakeNTech - 2021
+//Function to call from HTML. Loads the file and passes it to HTML_Formatter.
+function markdown(file_url){
+    if(file_url.split('.').pop()!="md"){
+        console.log("Bad Extension!");
+        document.getElementById("markdown_contents").innerHTML = "<p>Bad File Requested!</p>"
+        return;
+    }
+    fetch(file_url)
+    .then(res => res.blob())
+    .then(blob => {
+        var reader = new FileReader();
+        reader.onload = function() {
+            //console.log(reader.result);
+            HTML_Format(reader.result);
+        }
+        reader.readAsText(blob);
+    })       
+}
+// Formats the Markdown file and places on HTML
+function HTML_Format(text){
+    //console.log(text.split(/\r?\n/));
+    var text = text.split(/\r?\n/);
+    var i = 0;
+    var image_id = 0;
+    while(i < text.length){
+        // console.log(i)
+        // Deal with headings
+        if(text[i].includes("#")){
+            document.getElementById("markdown_contents").innerHTML += "<div id='header'> </div>"
+            if(text[i].split(" ")[0]==="#"){
+                document.getElementById("header").innerHTML += `<h1>`+text[i].replace("#","")+`</h1>`
+            }   
+            if(text[i].split(" ")[0]==="##"){
+                document.getElementById("header").innerHTML += `<p>`+text[i].replace("##","")+`</p>`
+            }
+            if(text[i].split(" ")[0]==="###"){
+                document.getElementById("header").innerHTML += `<h5>`+text[i].replace("###","")+`</h5>`
+            }
+        }
+        // Deal with code blocks
+        else if (text[i].includes("```")){
+            i++
+            document.getElementById("markdown_contents").innerHTML += `<div class='code_block' id='`+codeblock_no+`'> </div>`
+            while(text[i]!=="```"){
+                if(text[i]==="```"){
+                    i++
+                    break;
+                }
+                else{
+                    document.getElementById(codeblock_no).innerHTML += `<xmp>`+text[i].toString()+`</xmp>`
+                    i++
+                }
+            }
+            codeblock_no++
+        }
+        // Deal with images ![alt text](url)
+        else if(text[i].startsWith("!")){
+            var image_details = text[i].replace(/[!()]/g,",").split(",")
+            document.getElementById("markdown_contents").innerHTML += "<div class='picture_card' id='image_"+image_id+"'> </div>"
+            //Check to see if image has alt-text/caption
+            if(image_details[1]!=`[]`){
+                document.getElementById("image_"+image_id).innerHTML += `<img id='image' alt="`+image_details[1].replace("[","").replace("]","")+`" src='`+image_details[2]+`'>`
+                document.getElementById("image_"+image_id).innerHTML += `<div id="card_container"><p>`+image_details[1].replace("[","").replace("]","")+`</p>`
+            }
+            else{
+                document.getElementById("image_"+image_id).innerHTML += `<img id='image' src='`+image_details[2]+`'>`
+            }
+            image_id++
+        }
+        else if(text[i].startsWith("*")){
+            document.getElementById("markdown_contents").innerHTML += `<p><i>`+text[i].replace(/\*/g,"")+`<i></p>`;
+        }
+            // Deal with text
+        else{
+            document.getElementById("markdown_contents").innerHTML += `<p>`+text[i].replace("\\","")+`</p>`;
+        }
+        i++
+    }
+}
